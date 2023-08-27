26c363b2d5476e26bcfd40e8184c568e5b2ec008
Mesfin Girma <mesfingirmatwo@gmail.com>
2023-08-27 07:16:24 +0300

    fetched prducts from backend

--- lesson2/backend/server.js
+++ lesson2/backend/server.js
@@ -11,30 +11,41 @@ const port = 8080;
 const products = [
     {
           id: 1,
-          name: "product1",
-          catagory: "catagory1",
-          image: ""
+          name: "Nike Air Max 270 Men's Shoes",
+          catagoryId: "Shoes",
+          price:1200,
+          image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/41e147d8-6410-4b75-b46c-567b0d5903ca/air-max-270-mens-shoes-KkLcGR.png"
     },
     {
-        id: 2,
-        name: "product2",
-        catagory: "catagory2",
-        image: ""
-    }
-]
-const catagory = [
-  {
-        id: 1,
-        catagoryName: "shoes",
-        image: ""
+      id: 2,
+      name: "Men's Short Sleeve Shirt",
+      catagory: "cloth",
+      price:1500,
+      image: "https://m.media-amazon.com/images/I/71ZiB2HQbWL._AC_SY879._SX._UX._SY._UY_.jpg"
+  },
+    {
+        id: 3,
+        name: "Nike Air Max 95 Men's Shoes",
+        catagory: "Shoes",
+        price:1500,
+        image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f411a3ec-39b7-490e-b2c8-335264adb054/air-max-95-shoes-CwsxnW.png"
+    },
+    {
+      id: 4,
+      name: "Women's Shoes",
+      catagory: "Shoes",
+      price:1500,
+      image: "https://m.media-amazon.com/images/I/51FMSSQjt0S._AC_UX679_.jpg"
   },
   {
-      id: 2,
-      catagoryName: "cloth",
-      image: ""
-
-  }
+    id: 5,
+    name: "Women Short Sleeve Summer Midi Dress",
+    catagory: "Cloth",
+    price:1500,
+    image: "https://images.meesho.com/images/products/271743407/43qlw_512.webp"
+}
 ]
+
 app.get('/', (req, res) => {
     res.send('hello from backend')
   })
